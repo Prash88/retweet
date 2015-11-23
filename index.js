46a38562f8ef199792cee0ae57ca41cb721a67c0
Prashanth <prash88@iastate.edu>
2015-11-22 22:52:53 -0800

    Super super

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ function onTweet(tweet) {
     }, onReTweet);
 
     tu.update({
-        status: 'test'
+        status: '@itisprashanth idhu attack panra panda, idhu attract panra panda'
     }, onTweeted);
 }
 
