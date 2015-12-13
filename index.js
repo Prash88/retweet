df43dae3bbb09bfd5ea65edd1d0f2ea52bf9fec1
Prashanth <prash88@iastate.edu>
2015-12-12 21:28:33 -0800

    More change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var conf = {
 	    access_token_key: '4368073355-Vc6FiURUPh3nXFo2Xj0cVgdmV9SAyABXh8XiTAp',
 	    access_token_secret: 'OTFNiOUu2nvjiu68IrN0mxwRsfHqfv4KIASp7Qi0MEtgq'
 	},
-	terms: ['chennairainshelp','chennaifloods','chennairains']
+	terms: ['dmkfails','chennaifloods','chennairains']
 };
 
 // going to use the tuiter node module to get access to twitter
